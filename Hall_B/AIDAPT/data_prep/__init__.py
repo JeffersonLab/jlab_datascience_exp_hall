4d00123d46b8fd322a4ef3f2a23d2bf2acd8f884
Steven Goldenberg <sgolden@jlab.org>
2024-03-01 15:31:12 -0500

    Update __init__.py for new scalers

--- Hall_B/AIDAPT/data_prep/__init__.py
+++ Hall_B/AIDAPT/data_prep/__init__.py
@@ -4,3 +4,13 @@ register(
     id="lab_variables_to_invariants", 
     entry_point= "Hall_B.AIDAPT.data_parsers.lab_variables_to_invariants:LabVariablesToInvariants"
 )
+
+register(
+    id="numpy_standard_scaler", 
+    entry_point= "Hall_B.AIDAPT.data_parsers.numpy_standard_scaler:LabVariablesToInvariants"
+)
+
+register(
+    id="numpy_minmax_scaler", 
+    entry_point= "Hall_B.AIDAPT.data_parsers.numpy_minmax_scaler:NumpyMinMaxScaler"
+)
