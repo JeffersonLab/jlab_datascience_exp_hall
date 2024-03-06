6a8c7a71cbfd0c7a34ec02a3c4f23b3d1ec0d2b6
Steven Goldenberg <sgolden@jlab.org>
2024-03-06 14:11:06 -0500

    Update aidapt_numpy_reader_v0.py
    Added additional comments and swapped load/save_config functions with load/save. This module has no state, so the naming is a "matter of taste".

--- Hall_B/AIDAPT/data_parsers/aidapt_numpy_reader_v0.py
+++ Hall_B/AIDAPT/data_parsers/aidapt_numpy_reader_v0.py
@@ -4,7 +4,8 @@ from jlab_datascience_toolkit.core.jdst_data_parser import JDSTDataParser
 from Hall_B.AIDAPT.utils.config_utils import verify_config
 import yaml
 import inspect
-
+from pathlib import Path
+import os
 
 aidapt_numpy_reader_log = logging.getLogger('AIDAPT Parser V0 Logger')
 
@@ -12,9 +13,10 @@ aidapt_numpy_reader_log = logging.getLogger('AIDAPT Parser V0 Logger')
 class AIDAPTNumpyReaderV0(JDSTDataParser):
     """Reads a list of .npy files and concatenates them along a given axis.
 
-    Required intialization arguments: `config: dict`, `name: str`
+    Required intialization arguments: `config: dict`
+    Optional intialization arguments: `name: str`
 
-    Required configuration keys: `filepaths: str` or `list[str]`
+    Required configuration keys: `filepaths: str | list[str]`
     Optional configuration keys: `axis: int = 0`
 
     Attributes
@@ -26,25 +28,29 @@ class AIDAPTNumpyReaderV0(JDSTDataParser):
 
     Methods
     -------
-    load_config(path)
-        Loads and verifies a configuration dictionary from <path>
-    save_config(path)
-        Saves the current configuration dictionary to a yaml file located at 
-        <path>
+    get_info()
+        Prints this docstring
     load(path)
-        Currently does nothing
+        Loads this module from `path/self.module_name`
     save(path)
-        Currently does nothing
+        Saves this module from `path/self.module_name`
     load_data(path)
         Loads all files listed in `config['filepaths']` and concatenates them 
         along the `config['axis']` axis
     save_data(path)
-        Currently does nothing
+        Does nothing
+    load_config(path)
+        Does nothing
+    save_config(path)
+        Does nothing
 
     """
 
     def __init__(self, config: dict, name: str = "AIDAPT Numpy Reader V0"):
         self.module_name = name
+
+        # Required config keys contains only keys that do not have reasonable
+        #   defaults.
         self.required_config_keys = ['filepaths']
         verify_config(config, self.required_config_keys)
         self.config = config
@@ -57,25 +63,36 @@ class AIDAPTNumpyReaderV0(JDSTDataParser):
             self.config['axis'] = 0
 
     def get_info(self):
+        """ Prints the docstring for the AIDAPTNumpyReaderV0 module"""
         print(inspect.getdoc(self))
 
-    def load_config(self, path):
-        with open(path, 'r') as f:
-            config = yaml.safe_load(f)
-        verify_config(config, self.required_config_keys)
-        self.config = config
+    def load(self, path: str):
+        """ Load the entire module state from `path/<self.module_name>/`
 
-    def save_config(self, path):
-        with open(path, 'w') as f:
+        Args:
+            path (str): Path to folder containing a `self.module_name` folder.
+        """
+        base_path = Path(path)
+        save_dir = base_path.joinpath(self.module_name)
+        with open(save_dir.joinpath('config.yaml'), 'r') as f:
+            loaded_config = yaml.safe_load(f)
+
+        self.config.update(loaded_config)
+
+    # Save the entire module state to a folder under <path> called
+    # <self.module_name>
+    def save(self, path: str):
+        """Save the entire module state to a folder under `path` called 
+        `self.module_name`
+
+        Args:
+            path (str): Location to save the module folder
+        """
+        base_path = Path(path)
+        save_dir = base_path.joinpath(self.module_name)
+        os.makedirs(save_dir)
+        with open(save_dir.joinpath('config.yaml'), 'w') as f:
             yaml.safe_dump(self.config, f)
-
-    def load(self):
-        aidapt_numpy_reader_log.debug('Nothing to load...')
-        pass
-
-    def save(self):
-        aidapt_numpy_reader_log.debug('Nothing to save...')
-        pass
 
     def load_data(self):
         data_list = []
@@ -85,5 +102,12 @@ class AIDAPTNumpyReaderV0(JDSTDataParser):
 
         return np.concatenate(data_list, axis=self.config['axis'])
 
-    def save_data(self):
+    # Unimplemented functions below
+    def save_data(self, path: str):
+        pass
+
+    def load_config(self, path: str):
+        pass
+
+    def save_config(self, path: str):
         pass
