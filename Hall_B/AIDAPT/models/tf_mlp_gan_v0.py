5a10083127bb487aa7530c6a326b4af52b6a0a13
Steven Goldenberg <sgolden@jlab.org>
2024-03-18 12:09:55 -0400

    Major reformat of tf_mlp_gan_v0
    Now includes save and load functions and splits off portions of training that we may want to override in a subclass (like the train_step).

--- Hall_B/AIDAPT/models/tf_mlp_gan_v0.py
+++ Hall_B/AIDAPT/models/tf_mlp_gan_v0.py
@@ -3,46 +3,111 @@ import numpy as np
 from tqdm.auto import tqdm
 from jlab_datascience_toolkit.core.jdst_model import JDSTModel
 from Hall_B.AIDAPT.utils.config_utils import verify_config
+import matplotlib.pyplot as plt
+import inspect
+import yaml
+import os
 
-# X is vertex data with shape (N,5), Y is detector data with shape (N,4)
-# X is used to condition generator output, Y are the detector values we want to generate
-# Generator takes (N, X.shape[1]+latent_dim) while discriminator takes (N, Y.shape[1])
+def get_optimizer(type: str = 'Adam', optimizer_config: dict = None):
+    """Gets a non-legacy Optimizer instance
 
+    Args:
+        optimizer_config (dict, optional): Optional configuration dictionary. 
+            When not provided, returns an Adam optimizer with default settings.
+    Returns:
+        tf.keras.optimizer.Optimizer
+    """
+
+    if optimizer_config is None:
+        optimizer_config = dict()
+
+    module = 'keras.optimizers'
+    return tf.keras.optimizers.deserialize(dict(class_name=type, module=module, config=optimizer_config))
+
+
+def get_layer(type: str = 'Dense', layer_config: dict = None):
+    """Gets a non-legacy Layer instance
+
+    Args:
+        layer_config (dict): Optional configuration dictionary.
+            When not provided, returns a Dense layer with a single unit output and default settings.
+
+    Returns:
+        tf.keras.layers.Layer
+    """
+    if layer_config is None:
+        layer_config = dict(units=1)
+
+    module = 'keras.layers'
+    return tf.keras.layers.deserialize(dict(class_name=type, module=module, config=layer_config))
+
+
+def build_sequential_model(layers: list = None):
+    if layers is None:
+        raise RuntimeError('Cannot build model without a list of layers')
+
+    layer_list = []
+    for layer_config in layers:
+        # layer_config is a tuple of (type, config_dict)
+        type, config_dict = layer_config
+        layer_list.append(get_layer(type = type, layer_config=config_dict))
+    
+    return tf.keras.Sequential(layer_list)
 
 class TF_MLP_GAN_V0(JDSTModel):
-    def __init__(self, config, name='unfolding_model_v1') -> None:
+    def __init__(self, config: dict = None, name: str = 'unfolding_model_v1') -> None:
+        """_summary_
+
+        Args:
+            config (dict, optional): Dictionary of configuration options. 
+                Defaults to:
+                    latent_dim=100,
+                    generator_optimizer=None,
+                    discriminator_optimizer=None,
+                    label_shape=5,
+                    image_shape=4,
+                    generator_layers=None,
+                    discriminator_layers=None,
+                    epochs=1,
+                    batch_size=32
+
+                
+            name (str, optional): Name of Module. Defaults to 'unfolding_model_v1'.
+        """
         self.module_name = name
-        self.required_cfg_keys = ['latent_dim', 'optimizer_lr',
-                                  'optimizer_beta1', 'label_shape',
-                                  'image_shape', 'generator_layers',
-                                  'discriminator_layers', 'batch_size',
-                                  'epochs']
-        verify_config(config, self.required_cfg_keys)
-        self.config = config
 
-        self.latent_dim = self.config[f'latent_dim']
-        self.optimizer_lr = self.config[f'optimizer_lr']
-        self.optimizer_beta1 = self.config[f'optimizer_beta1']
-        self.generator_layers = self.config[f'generator_layers']
-        self.discriminator_layers = self.config[f'discriminator_layers']
-        self.batch_size = self.config[f'batch_size']
-        self.epochs = self.config[f'epochs']
+        # Set config defaults
+        self.config = dict(
+            latent_dim=100,
+            generator_optimizer=None,
+            discriminator_optimizer=None,
+            label_shape=5,
+            image_shape=4,
+            generator_layers=None,
+            discriminator_layers=None,
+            epochs=1,
+            batch_size=32,
+            random_seed=42)
+        
+        if config is not None:
+            self.config.update(config)
 
-        self.discriminator_optimizer = tf.keras.optimizers.legacy.Adam(
-            self.optimizer_lr, self.optimizer_beta1)
+        self.set_model_variables()
 
-        self.generator_optimizer = tf.keras.optimizers.legacy.Adam(
-            5*self.optimizer_lr, self.optimizer_beta1)
+        self.rng = tf.random.Generator.from_seed(self.config['random_seed'])
 
+        # TODO: Make sure random_seed also controls model weights/optimizers/etc
+
+        self.discriminator_optimizer = get_optimizer(
+            *self.config['discriminator_optimizer'])
+
+        self.generator_optimizer = get_optimizer(
+            *self.config['generator_optimizer'])
+
+        #TODO: Make this configurable in the config
         self.gen_loss_fn = tf.keras.losses.MeanSquaredError()
         self.disc_loss_fn = tf.keras.losses.MeanSquaredError()
         self.acc_fn = tf.keras.metrics.Accuracy()
-        # X-shape
-        # self.input_shape = self.config[f'input_shape']
-        self.image_shape = self.config[f'image_shape']
-        # Y-shape
-        # self.output_shape = self.config[f'output_shape']
-        self.label_shape = self.config[f'label_shape']
 
         self.discriminator = self.build_discriminator()
         self.discriminator.summary()
@@ -51,48 +116,143 @@ class TF_MLP_GAN_V0(JDSTModel):
 
         self.generator = self.build_generator()
         self.generator.summary()
-
         self.generator.compile(loss='mse', optimizer=self.generator_optimizer)
 
-        # # The generator takes noise as input and vertex_events
-        # generator_vertex = tf.keras.layers.Input(shape=self.input_shape)
-        # generator_noise = tf.keras.layers.Input(shape=(self.latent_dim,))
-        # Det_events = self.generator([generator_vertex, generator_noise])
-
-        # # For the combined model we will only train the generator
-        # self.discriminator.trainable = False
-
-        # # The valid takes generated detector_events as input and determines validity
-        # valid = self.discriminator([generator_vertex, Det_events])
-
-        # # The combined model  (stacked generator and discriminator)--->Trains generator to fool discriminator
-        # self.combined = tf.keras.models.Model(
-        #     [generator_vertex, generator_noise], valid)
-
-        # # (!!!) Optimize w.r.t. MSE loss instead of crossentropy
-        # # MMD_loss = self.MMD_loss()
-        # self.combined.compile(loss='mse', optimizer=self.generator_optimizer)
+    def set_model_variables(self):
+        self.latent_dim = self.config[f'latent_dim']
+        self.generator_layers = self.config[f'generator_layers']
+        self.discriminator_layers = self.config[f'discriminator_layers']
+        self.batch_size = self.config[f'batch_size']
+        self.epochs = self.config[f'epochs']
+        self.image_shape = self.config[f'image_shape']
+        self.label_shape = self.config[f'label_shape']
 
     def get_info(self):
-        print(f'Module name == {self.module_name}')
-        print('tf.keras.models.Model will predict in forward() and should be trained prior to use')
+        print(inspect.getdoc(self))
 
-    def load(self):
-        pass
+    def load(self, filepath):
+        # Set all configuration settings
+        with open(os.path.join(filepath, 'config.yaml'), 'r') as f:
+            self.config.update(yaml.safe_load(f))
+        self.set_model_variables()
 
-    def save(self):
-        # self.generator.save_weights('GEN_inv_innerGAN_lessarch_withgamma.h5')
-        # self.discriminator.save_weights('DES_inv_innerGAN_lessarch_withgamma.h5')
-        pass
+        # Build correct network before loading weights
+        self.generator = self.build_generator()
+        self.discriminator = self.build_discriminator()
+
+        # Load network weights
+        self.generator.load_weights(os.path.join(filepath, 'generator_weights'))
+        self.discriminator.load_weights(os.path.join(filepath, 'discriminator_weights'))
+
+        # TODO load optimizers specified in config...
+
+    def save(self, filepath):
+        self.generator.save_weights(os.path.join(filepath, 'generator_weights'))
+        self.discriminator.save_weights(os.path.join(filepath, 'discriminator_weights'))
+        with open(os.path.join(filepath, 'config.yaml'), 'w') as f:
+            yaml.safe_dump(self.config, f)
+        # TODO: Save state of optimizers
 
     def load_config(self):
         return super().load_config()
-    
+
     def save_config(self):
         return super().save_config()
-    
+
     def analysis(self):
-        return super().analysis()
+        # Plot training history
+        plt.semilogy(self.history['g_loss']*4, label='Generator')
+        plt.semilogy(self.history['d_loss_real']*4, label='Discriminator Real')
+        plt.semilogy(self.history['d_loss_fake']*4, label='Discriminator Fake')
+        plt.axhline(y=1)
+        plt.ylabel('Loss')
+        plt.xlabel('Epochs')
+        plt.legend()
+        plt.show() 
+
+    # data is [images, labels]
+    def train(self, data):
+        images, labels = data
+        batch_size = self.batch_size
+        epochs = self.epochs
+
+        pbar = tqdm(range(epochs))
+        # d_loss (total, real, fake), #accuracy (real, fake), and #g_loss
+        metric_array = tf.zeros((epochs, 6))
+
+        for epoch in pbar:
+            metrics = self.train_step(images, labels)
+            metric_array[epoch, :] = metrics
+
+            pbar.set_postfix_str(
+                f'Acc Real/Fake: {metrics[3]:.3f}/{metrics[4]:.3f}, G_loss: {metrics[5]:.3f}')
+
+        self.history = {}
+        metric_names = ['d_loss_total', 'd_loss_real', 'd_loss_fake', 'acc_real', 'acc_fake', 'g_loss']
+        for metric, name in zip(metric_array.T, metric_names):
+            self.history[name] = metric
+        return self.history
+
+    def predict(self, data):
+        noise = np.random.normal(0, 1, (data.shape[0], self.latent_dim))
+        return self.generator.predict([data, noise], batch_size=1024)
+
+    def build_generator(self):
+        label = tf.keras.layers.Input(shape=(self.label_shape,))
+        noise = tf.keras.layers.Input(shape=(self.latent_dim,))
+        x = tf.keras.layers.Concatenate()([label, noise])
+
+        model = build_sequential_model(self.config['generator_layers'])
+        
+        x = model(x)
+
+        output = tf.keras.layers.Dense(self.image_shape, activation='tanh')(x)
+
+        generator = tf.keras.models.Model(
+            inputs=[label, noise], outputs=[output])
+        return generator
+
+    def build_discriminator(self):
+        label = tf.keras.layers.Input(shape=(self.label_shape,))
+        image = tf.keras.layers.Input(shape=(self.image_shape,))
+
+        x = tf.keras.layers.Concatenate()([label, image])
+
+        model = build_sequential_model(self.config['discriminator_layers'])
+        x = model(x)
+
+        output = tf.keras.layers.Dense(1, activation='sigmoid')(x)
+        discriminator = tf.keras.models.Model(
+            inputs=[label, image], outputs=output)
+        return discriminator
+
+    def train_step(self, images, labels):
+        batch_size = self.batch_size
+
+        noise = self.rng.normal((batch_size, 1), 0, 0.1)
+        valid = tf.ones((batch_size, 1)) + noise
+        fake = tf.zeros((batch_size, 1))
+        idx = np.random.randint(0, images.shape[0], batch_size)
+        img_batch, label_batch = images[idx], labels[idx]
+
+        # --->  Sample noise as generator input
+        noise = np.random.normal(0, 1, (batch_size, self.latent_dim))
+
+        # --->  Generate a batch of fake detector_events
+        gen_imgs = self.generator.predict(
+            [label_batch, noise], verbose=0)
+
+        gen_imgs = tf.stop_gradient(gen_imgs)
+
+        d_loss_real, acc_real = self.train_discriminator(
+            label_batch, img_batch, valid)
+        d_loss_fake, acc_fake = self.train_discriminator(
+            label_batch, gen_imgs, fake)
+        d_loss_total = (d_loss_real+d_loss_fake)/2
+
+        g_loss = self.train_generator(label_batch, noise, valid)
+
+        return [d_loss_total, d_loss_real, d_loss_fake, acc_real, acc_fake, g_loss]
 
     @tf.function
     def train_generator(self, labels, noise, valid):
@@ -115,77 +275,3 @@ class TF_MLP_GAN_V0(JDSTModel):
             zip(grads, self.discriminator.trainable_weights))
         acc = self.acc_fn(valid, predictions)
         return d_loss, acc
-
-    # data is [images, labels]
-    def train(self, data):
-        images, labels = data
-        batch_size = self.batch_size
-        epochs = self.epochs
-
-        valid = tf.ones((batch_size, 1)) + tf.random.normal((batch_size,1),0,0.1)
-        fake = tf.zeros((batch_size, 1))
-
-        pbar = tqdm(range(epochs))
-        # d_loss (total, real, fake), #accuracy (real, fake), and #g_loss
-        metric_array = np.zeros((epochs, 6))
-
-        for epoch in pbar:
-            # ---> Select a random batch of data
-            idx = np.random.randint(0, images.shape[0], batch_size)
-            img_batch, label_batch = images[idx], labels[idx]
-
-            # --->  Sample noise as generator input
-            noise = np.random.normal(0, 1, (batch_size, self.latent_dim))
-
-            # --->  Generate a batch of fake detector_events
-            gen_imgs = self.generator.predict(
-                [label_batch, noise], verbose=0)
-
-            gen_imgs = tf.stop_gradient(gen_imgs)
-
-            d_loss_real, acc_real = self.train_discriminator(label_batch, img_batch, valid)
-            d_loss_fake, acc_fake = self.train_discriminator(label_batch, gen_imgs, fake)
-            d_loss_total = (d_loss_real+d_loss_fake)/2
-
-            g_loss = self.train_generator(label_batch, noise, valid)
-            metric_array[epoch, :] = [d_loss_total, d_loss_real,
-                                      d_loss_fake, acc_real, acc_fake, g_loss]
-
-            # pbar.set_postfix_str(f'Metrics: {metric_array[epoch, :]}')
-            pbar.set_postfix_str(f'Acc Real/Fake: {acc_real:.3f}/{acc_fake:.3f}, G_loss: {g_loss:.3f}')
-
-        return metric_array
-
-    def predict(self, data):
-        noise = np.random.normal(0, 1, (data.shape[0], self.latent_dim))
-        return self.generator.predict([data, noise], batch_size=1024)
-
-    def build_generator(self):
-        label = tf.keras.layers.Input(shape=(self.label_shape,))
-        noise = tf.keras.layers.Input(shape=(self.latent_dim,))
-        visible = tf.keras.layers.Concatenate()([label, noise])
-
-        x = visible
-        for layer in self.generator_layers:
-            x = tf.keras.layers.Dense(units=layer.units,
-                                      activation=layer.activation, **layer.kwargs)(x)
-            x = tf.keras.layers.BatchNormalization(momentum=0.8)(x)
-
-        output = tf.keras.layers.Dense(self.image_shape, activation='tanh')(x)
-
-        generator = tf.keras.models.Model(inputs=[label, noise], outputs=[output])
-        return generator
-
-    def build_discriminator(self):
-        label = tf.keras.layers.Input(shape=(self.label_shape,))
-        image = tf.keras.layers.Input(shape=(self.image_shape,))
-
-        x = tf.keras.layers.Concatenate()([label, image])
-        for layer in self.discriminator_layers:
-            x = tf.keras.layers.Dense(units=layer.units,
-                                      activation=layer.activation, **layer.kwargs)(x)
-
-        output = tf.keras.layers.Dense(1, activation='sigmoid')(x)
-        discriminator = tf.keras.models.Model(
-            inputs=[label, image], outputs=output)
-        return discriminator
