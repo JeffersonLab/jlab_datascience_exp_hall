Fixed assortment of bugs, issues with scaling of data, and outer GAN functionality.

Hall_B/AIDAPT/aidapt_toolkit/data_prep/numpy_minmax_scaler.py
@@ -42,6 +42,7 @@ class NumpyMinMaxScaler(JDSTDataPrep):
 
     def train(self, data: np.ndarray):
         x = data
+        
         if data.ndim == 1:
             x = x[:, np.newaxis]
 
@@ -58,7 +59,6 @@ class NumpyMinMaxScaler(JDSTDataPrep):
         x = data
         if data.ndim == 1:
             x = x[:, np.newaxis]
-
         data_zero_one = (x - self.data_min) / self.data_range
         data_scaled = data_zero_one * \
             (self.feature_range[1] - self.feature_range[0])
@@ -72,6 +72,7 @@ class NumpyMinMaxScaler(JDSTDataPrep):
 
         data_zero_one = (x - self.feature_range[0]) / \
             (self.feature_range[1] - self.feature_range[0])
+        #data_unscaled = data_zero_one * self.data_range[:-1] + self.data_min[:-1]
         data_unscaled = data_zero_one * self.data_range + self.data_min
         return data_unscaled
 

Hall_B/AIDAPT/aidapt_toolkit/drivers/hydra_driver.py
@@ -11,7 +11,7 @@ import logging
 
 logging.getLogger("matplotlib").setLevel(logging.INFO)
 
-
+#hydra_basic_config
 @hydra.main(
     version_base=None, config_path="../configs", config_name="hydra_basic_config"
 )
@@ -38,45 +38,56 @@ def run(config) -> None:
     v_scaler = aidapt_toolkit.data_prep.make(
         config["v_scaler"]["id"], config=config["v_scaler"], name="vertex_scaler"
     )
+    v_s_scaler = aidapt_toolkit.data_prep.make(
+        config["v_scaler"]["id"], config=config["v_scaler"], name="vertex_scaler"
+    )
     model = aidapt_toolkit.models.make(
         config["model"]["id"], config=config["model"], name="mlp_gan_model"
     )
 
     v_data = vertex_parser.load_data()
     d_data = detector_parser.load_data()
+    #print("v_data.shape: ", v_data.shape)
 
     v_invariants = lab2inv_prep.run(v_data)
     d_invariants = lab2inv_prep.run(d_data)
 
+    v_s_invariant = v_invariants[:,4]
     d_invariants = d_invariants[:, :-1]
     v_invariants = v_invariants[:, :-1]
 
+    v_s_scaler.train(v_s_invariant)
     v_scaler.train(v_invariants)
     d_scaler.train(d_invariants)
 
+    v_s_invariant_scaled = v_s_scaler.run(v_s_invariant)
     v_invariants_scaled = v_scaler.run(v_invariants)
     d_invariants_scaled = d_scaler.run(d_invariants)
 
     batches_per_epoch = len(d_invariants_scaled) // config['model']['batch_size']
     batches_per_epoch_remainder = len(d_invariants_scaled) % config['model']['batch_size']
+    #batches_per_epoch = len(d_invariants) // config['model']['batch_size']
+    #batches_per_epoch_remainder = len(d_invariants) % config['model']['batch_size']
     #print('batches_per_epoch_remainder: ', batches_per_epoch_remainder)
     if batches_per_epoch_remainder > 0:
         batches_per_epoch += 1
         
+    
     #history = model.train([d_invariants_scaled, v_invariants_scaled])
-    history = model.train([d_invariants_scaled, v_invariants_scaled],
-                    save_path, batches_per_epoch, d_invariants, d_scaler,
+    history = model.train([d_invariants_scaled, v_s_invariant_scaled],
+                    save_path, batches_per_epoch, d_invariants_scaled, d_scaler,
                     config['model']['latent_dim'],
                     config['metrics']['layer_specific_gradients'], config['metrics']['grad_frequency'],
                     config['metrics']['chi2'], config['metrics']['chi2_frequency'],
                     config['metrics']['disc_accuracy'], config['metrics']['acc_frequency'])
-    
-    d_results_scaled = model.predict(v_invariants_scaled)
 
+    d_results_scaled = model.predict(v_s_invariant_scaled)
     d_results = d_scaler.reverse(d_results_scaled)
 
     # Plot training history
     #model.analysis(save_path)
+    #d_invariants_plot = np.delete(d_invariants, 3, axis=1)
+    #d_invariants_scaled = np.delete(d_invariants_scaled, 3, axis=1)
 
     # Plot distributions
     fig, axs = plt.subplots(2, 2)
@@ -166,8 +177,8 @@ def run(config) -> None:
     vertex_parser.save(os.path.join(save_path, "vertex_parser"))
     detector_parser.save(os.path.join(save_path, "detector_parser"))
     lab2inv_prep.save(os.path.join(save_path, "lab2inv_prep"))
-    d_scaler.save(os.path.join(save_path, "d_scaler"))
-    v_scaler.save(os.path.join(save_path, "v_scaler"))
+    #d_scaler.save(os.path.join(save_path, "d_scaler"))
+    #v_scaler.save(os.path.join(save_path, "v_scaler"))
     model.save(os.path.join(save_path, "cgan_model"))
 
 

Hall_B/AIDAPT/aidapt_toolkit/drivers/outer_gan_driver.py
@@ -30,11 +30,18 @@ def run(config):
         config=config["detector_parser"],
         name="detector_parser",
     )
-    #detector_folding_parser = aidapt_toolkit.data_parsers.make(
-    #    config["detector_folding_parser"]["id"],
-    #    config=config["detector_folding_parser"],
-    #    name="detector_folding_parser",
-    #)
+    '''
+    detector_ps_parser = aidapt_toolkit.data_parsers.make(
+        config["detector_ps_parser"]["id"],
+        config=config["detector_ps_parser"],
+        name="detector_ps_parser",
+    )
+    vertex_ps_parser = aidapt_toolkit.data_parsers.make(
+        config["vertex_ps_parser"]["id"],
+        config=config["vertex_ps_parser"],
+        name="vertex_ps_parser",
+    )
+    '''
     lab2inv_prep = aidapt_toolkit.data_prep.make(
         config["lab2inv"]["id"],
         config=config["lab2inv"],
@@ -45,31 +52,49 @@ def run(config):
     v_scaler = aidapt_toolkit.data_prep.make(
         config["v_scaler"]["id"], config=config["v_scaler"], name="vertex_scaler"
     )
+    v_s_scaler = aidapt_toolkit.data_prep.make(
+        config["v_scaler"]["id"], config=config["v_scaler"], name="vertex_scaler"
+    )
+    '''
+    d_ps_scaler = aidapt_toolkit.data_prep.make(
+        config["d_scaler"]["id"], config=config["d_scaler"], name="detector_scaler"
+    )
+    v_ps_scaler = aidapt_toolkit.data_prep.make(
+        config["v_scaler"]["id"], config=config["v_scaler"], name="vertex_scaler"
+    )
+    '''
     model = aidapt_toolkit.models.make(
         config["model"]["id"], config=config["model"], name="mlp_gan_model"
     )
 
     v_data = vertex_parser.load_data()
     d_data = detector_parser.load_data()
-    #d_folding_data = detector_folding_parser.load_data()
+    #v_ps_data = vertex_ps_parser.load_data()
+    #d_ps_data = detector_ps_parser.load_data()
 
     v_invariants = lab2inv_prep.run(v_data)
     d_invariants = lab2inv_prep.run(d_data)
-    #print("v_invariants[:,4]: ", v_invariants[:,4])
-    #d_folding_invariants = lab2inv_prep.run(d_folding_data)
- 
-    #v_invariants = v_invariants[:, :-1]
-    #d_invariants = d_invariants[:, :-1]
-    #d_gen_invariants = d_gen_invariants[:, :-1]
+    #v_ps_invariants = lab2inv_prep.run(v_ps_data)
+    #d_ps_invariants = lab2inv_prep.run(d_ps_data)
 
+    v_s_invariant = v_invariants[:,4]
+    d_invariants = d_invariants[:, :-1]
+    v_invariants = v_invariants[:, :-1]
+    #d_ps_invariants = d_ps_invariants[:, :-1]
+    #v_ps_invariants = v_ps_invariants[:, :-1]
+
+    v_s_scaler.train(v_s_invariant)
     v_scaler.train(v_invariants)
     d_scaler.train(d_invariants)
-    #d_scaler.train(d_folding_invariants)
+    #v_ps_scaler.train(v_ps_invariants)
+    #d_ps_scaler.train(d_ps_invariants)
 
+    v_s_invariant_scaled = v_s_scaler.run(v_s_invariant)
     v_invariants_scaled = v_scaler.run(v_invariants)
-    #v_invariants_scaled = v_invariants_scaled[: len(d_invariants)]
     d_invariants_scaled = d_scaler.run(d_invariants)
-    #d_folding_invariants_scaled = d_scaler.run(d_folding_invariants)
+    #v_ps_invariants_scaled = v_ps_scaler.run(v_ps_invariants)
+    #d_ps_invariants_scaled = d_ps_scaler.run(d_ps_invariants)
+
     # Scale (based on phase space data!)
         # These are completely different files... Do we want to load them and calculate
         # the mean and standard deviation or do we want to just have them in the config?
@@ -77,23 +102,20 @@ def run(config):
 
     batches_per_epoch = len(d_invariants_scaled) // config['model']['batch_size']
     batches_per_epoch_remainder = len(d_invariants_scaled) % config['model']['batch_size']
-    #print('batches_per_epoch_remainder: ', batches_per_epoch_remainder)
+    
     if batches_per_epoch_remainder > 0:
         batches_per_epoch += 1
         
     # Train the model
-    history = model.train([d_invariants_scaled, v_invariants_scaled],
-                    save_path, batches_per_epoch, d_invariants, d_scaler,
+    history = model.train([d_invariants_scaled, [v_invariants_scaled, v_s_invariant_scaled]],
+                    save_path, batches_per_epoch, d_invariants_scaled, d_scaler,
                     config['model']['latent_dim'],
                     config['metrics']['layer_specific_gradients'], config['metrics']['grad_frequency'],
                     config['metrics']['chi2'], config['metrics']['chi2_frequency'],
                     config['metrics']['disc_accuracy'], config['metrics']['acc_frequency'])
     
-    v_s_scaled = v_invariants_scaled[:, -1]
-    #d_results_scaled = model.predict(v_invariants_scaled)
-    unf_results_scaled = model.predict(v_s_scaled)
-    d_results_scaled = model.predict_full(v_invariants_scaled)
-    #d_results = d_results_scaled
+    d_results_scaled = model.predict_full(v_s_invariant_scaled)
+    unf_results_scaled = model.predict(v_s_invariant_scaled)
     d_results = d_scaler.reverse(d_results_scaled)
     unf_results = d_scaler.reverse(unf_results_scaled)
     
@@ -119,6 +141,7 @@ def run(config):
             label="Detector",
         )
         ax.set_xlabel(name)
+        '''
         ax.hist(
             unf_results[:, i],
             bins=100,
@@ -127,11 +150,12 @@ def run(config):
             density=True,
             label="Unf_GAN",
         )
+        '''
         ax.set_xlabel(name)
     ax.legend()
     fig.tight_layout()
     fig.savefig(os.path.join(save_path, "distributions.png"))
-    
+    '''
     fig, axs = plt.subplots(2, 2)
     axs = axs.flat
     output_names = ("sppim", "spipm", "tpip", "alpha")
@@ -181,7 +205,7 @@ def run(config):
     ax.legend()
     fig.tight_layout()
     fig.savefig(os.path.join(save_path, "distributions_before_training.png"))
-    
+    '''
     # Plot reconstruction errors
     fig, ax = plt.subplots(1, 1)
     p_rec_gan = np.sqrt(
@@ -214,7 +238,7 @@ def run(config):
         loc="upper left",
     )
     fig.savefig(os.path.join(save_path, "reconstruction_errors.png"))
-
+    
     # Save modules
     vertex_parser.save(os.path.join(save_path, "vertex_parser"))
     detector_parser.save(os.path.join(save_path, "detector_parser"))

Hall_B/AIDAPT/aidapt_toolkit/models/tf_outer_cgan_v0.py
@@ -16,21 +16,22 @@ class TF_OuterGAN_V0(TF_CGAN_Keras):
 
     
     """
-    def __init__(self, unfolding_path, unfolding_id, discriminator, generator, noise_dim = 100, batch_size = 32) -> None:
+    def __init__(self, discriminator, generator, folding_model, noise_dim = 100, batch_size = 32) -> None:
         super().__init__(discriminator, generator, noise_dim = 100, batch_size = 32)
+        '''
+        self.folding_path = folding_path
+        self.folding_id = folding_id
 
-        self.unfolding_path = unfolding_path
-        self.unfolding_id = unfolding_id
+        with open(f"{self.folding_path}/config.yaml", "r") as f:
+            self.folding_config = yaml.safe_load(f)
 
-        with open(f"{self.unfolding_path}/config.yaml", "r") as f:
-            self.unfolding_config = yaml.safe_load(f)
-
-        self.unfolding_model = make(self.unfolding_id, config=self.unfolding_config)
-        self.unfolding_model.load_inner_GAN(self.unfolding_path)
+        self.folding_model = make(self.folding_id, config=self.folding_config)
+        self.folding_model.load_inner_GAN(self.folding_path)
 
         # This assumes InnerGAN is a TF_CGAN...
-        self.unfolding_model.cgan.trainable = False
-
+        self.folding_model.cgan.trainable = False
+        '''
+        self.folding_model = folding_model
         # Initialize loss and overall gradient trackers
         self.d_loss_tracker = tf.keras.metrics.Mean(name='d_loss')
         self.g_loss_tracker = tf.keras.metrics.Mean(name='g_loss')
@@ -70,85 +71,43 @@ class TF_OuterGAN_V0(TF_CGAN_Keras):
     
     def train_step(self, data):
         inputs, real_images = data
-        #real_images, inputs = data
-        #print("inputs: ", inputs)
-        #print("inputs.shape: ", inputs.shape)
         vertex_s = inputs[:,4]
-        #print("vertex_s: ", vertex_s)
-        #print("min(vertex_W): ", np.sqrt(np.min(vertex_s)))
-        #print("min(vertex_W): ", tf.reduce_min(vertex_s))
-        #print("min(vertex_W): ", tf.sqrt(tf.reduce_min(vertex_s)))
-        #print("max(vertex_W): ", max(vertex_s))
-        #inputs = inputs[:,:-1]
-        #real_images = real_images[:,:-1]
         inputs = inputs[:,:4]
-        real_images = real_images[:,:4]
-        #batch_size = tf.shape(inputs)[0]
         batch_size = tf.shape(vertex_s)[0]
         noise = tf.random.normal(shape=(batch_size, self.noise_dim))
 
-        #generated_images = self.generator((inputs, noise))
+        labels = tf.concat([tf.zeros((batch_size, 1)), tf.ones((batch_size, 1))], axis=0)
 
-        #generated_images = self.generator((vertex_s, noise))
-        
-        #zeros_column = tf.zeros((batch_size, 1), dtype=generated_images.dtype)
-        #generated_images_padded = tf.concat([generated_images, zeros_column], axis=1)
-        #generated_images = self.unfolding_model.predict_graph_mode(generated_images)
-
-        #inner_noise = tf.random.normal(shape=(tf.shape(generated_images)[0], self.unfolding_model.latent_dim))
-        #inner_generated_images = self.unfolding_model.static_generator([generated_images, inner_noise], training=False)
-
-        #dummy_noise = tf.zeros_like(noise)
-        #inner_generated_images = self.unfolding_model.static_generator([generated_images, dummy_noise], training=False)
-
-        #combined_images = tf.concat([generated_images, real_images], axis=0)
-
-        #combined_images = tf.concat([inner_generated_images, real_images], axis=0)
-        #combined_inputs = tf.concat([inputs, inputs], axis=0)
-
-        labels = tf.concat([tf.ones((batch_size, 1)), tf.zeros((batch_size, 1))], axis=0)
-        #labels = tf.concat([tf.zeros((batch_size, 1)), tf.ones((batch_size, 1))], axis=0)
-        #print("labels.shape: ", labels.shape)
-
-        # Store the true labels for accuracy calculation before adding noise  
+        # Store the true labels for accuracy calculation before adding noise
         true_labels = labels
         
-        #labels += 0.05*tf.random.uniform(tf.shape(labels))
+        labels += 0.05*tf.random.uniform(tf.shape(labels))
 
-        disc_updates = 5
+        disc_updates = 1
         #Train discriminator 
         for _ in range(disc_updates):
             with tf.GradientTape(persistent=True) as disc_tape:
+
+                # Generate unfolding GAN generator output 
                 generated_images = self.generator((vertex_s, noise))
-                inner_noise = tf.random.normal(shape=(tf.shape(generated_images)[0], self.unfolding_model.latent_dim))
-                inner_generated_images = self.unfolding_model.static_generator([generated_images, inner_noise])#, training=False)
-                #print("inner_generated_images.shape: ", inner_generated_images.shape)
-                #print("real_images.shape: ", real_images.shape)
+
+                # Generate inner GAN generator output 
+                inner_noise = tf.random.normal(shape=(tf.shape(generated_images)[0], self.folding_model.latent_dim))
+                inner_generated_images = self.folding_model.static_generator([generated_images, inner_noise], training=False)
+
+                # Prepare training data
                 combined_images = tf.concat([inner_generated_images, real_images], axis=0)
-                #combined_images = tf.concat([generated_images, real_images], axis=0)
-                combined_inputs = tf.concat([inputs, inputs], axis=0)
-                predictions = self.discriminator((combined_inputs, combined_images))#, training=True)
-                #predictions = self.discriminator(combined_images, training=True)
+                combined_inputs = tf.concat([vertex_s, vertex_s], axis=0)
+
+                predictions = self.discriminator((combined_inputs, combined_images), training=True)
                 d_loss = self.d_loss_fn(labels, predictions)
             discriminator_gradients = disc_tape.gradient(d_loss, self.discriminator.trainable_weights)
             self.d_optimizer.apply_gradients(
                 zip(discriminator_gradients, self.discriminator.trainable_weights)
             )
-        #print("len(discriminator_gradients): ", len(discriminator_gradients))
-        '''
-        print("Discriminator trainable weights:")
-        for w in self.discriminator.trainable_weights:
-            print(w.name, w.shape)
-        print("Inner generator trainable weights:")
-        for sg in self.unfolding_model.static_generator.trainable_weights:
-            print(sg.name, sg.shape)
-        print("Generator trainable weights:")
-        for w in self.generator.trainable_weights:
-            print(w.name, w.shape)
-        '''
+
         # If using logits, apply sigmoid to get probabilities
         if isinstance(self.d_loss_fn, tf.keras.losses.BinaryCrossentropy) and not getattr(self.d_loss_fn, 'from_logits', False):
-            #print("No action needed")
             pass  # No action needed
         else:
             # Apply sigmoid if predictions are logits
@@ -165,44 +124,30 @@ class TF_OuterGAN_V0(TF_CGAN_Keras):
 
         # Calculate discriminator gradient norms
         discriminator_agg_gradient_norm = tf.sqrt(sum([tf.reduce_sum(tf.square(g)) for g in discriminator_gradients]))
-        #print("discriminator_agg_gradient_norm: ", discriminator_agg_gradient_norm)
 
         # Sample random points in the latent space
         random_latent_vectors = tf.random.normal(shape=(batch_size, self.noise_dim))
 
         # Assemble labels that say "all real images"
-        misleading_labels = tf.zeros((batch_size, 1))
-        #print("inner_generated_images: ", inner_generated_images)
-        # Train the generator (note that we should *not* update the weights
-        # of the discriminator)!
+        misleading_labels = tf.ones((batch_size, 1))
+
+        # Train the generator (note that we should *not* update the weights of the discriminator)!
         with tf.GradientTape() as gen_tape:
-            #generated_images = self.generator((inputs, random_latent_vectors))
-            generated_images = self.generator((vertex_s, random_latent_vectors))
-            #inner_generated_images = self.unfolding_model.predict_graph_mode(generated_images)
-            noise = tf.random.normal(shape=(tf.shape(generated_images)[0], self.unfolding_model.latent_dim))
-            inner_generated_images = self.unfolding_model.static_generator([generated_images, noise])#, training=False)
-            #tf.print("inner_generated_images: ", inner_generated_images)
-            #dummy_noise = tf.zeros_like(noise)
-            #inner_generated_images = self.unfolding_model.static_generator([generated_images, dummy_noise], training=False)
-            #predictions = self.discriminator((inputs, generated_images))#, training=False)
-            predictions = self.discriminator((inputs, inner_generated_images))#, training=False)
+            
+            # Generate unfolding GAN generator output 
+            generated_images = self.generator((vertex_s, random_latent_vectors), training=True)
+            
+            # Generate inner GAN generator output 
+            noise = tf.random.normal(shape=(tf.shape(generated_images)[0], self.folding_model.latent_dim))
+            inner_generated_images = self.folding_model.static_generator([generated_images, noise], training=False)
+            
+            # Generate unfolding GAN discriminator output
+            predictions = self.discriminator((vertex_s, inner_generated_images), training=False)
             g_loss = self.g_loss_fn(misleading_labels, predictions)      
+            
         generator_gradients = gen_tape.gradient(g_loss, self.generator.trainable_weights)
-        #for var, grad in zip(self.generator.trainable_weights, generator_gradients):
-        #    print(f"Variable: {var.name}, Gradient: {grad}")
         self.g_optimizer.apply_gradients(zip(generator_gradients, self.generator.trainable_weights))
-        #print("len(generator_gradients): ", len(generator_gradients))
-        '''
-        print("Discriminator trainable weights:")
-        for w in self.discriminator.trainable_weights:
-            print(w.name, w.shape)
-        print("Inner generator trainable weights:")
-        for sg in self.unfolding_model.static_generator.trainable_weights:
-            print(sg.name, sg.shape)
-        print("Generator trainable weights:")
-        for w in self.generator.trainable_weights:
-            print(w.name, w.shape)
-        '''
+
         gen_layer_grad_norms = []
         for idx, grad in enumerate(generator_gradients):
             norm = tf.norm(grad)
@@ -210,7 +155,6 @@ class TF_OuterGAN_V0(TF_CGAN_Keras):
 
         # Calculate generator gradient norms
         generator_agg_gradient_norm = tf.sqrt(sum([tf.reduce_sum(tf.square(g)) for g in generator_gradients]))
-        #print("generator_agg_gradient_norm: ", generator_agg_gradient_norm)
 
         # ----------------------------- Update tracked metrics ------------------------------- #
         self.d_loss_tracker.update_state(d_loss)

Hall_B/AIDAPT/aidapt_toolkit/utils/gradient_monitor.py
@@ -41,8 +41,6 @@ class GradientMonitor(tf.keras.callbacks.Callback):
             for layer_name, tracker in self.model.discriminator_layer_gradient_norms_tracker.items():
             #for layer in self.model.discriminator_layer_gradient_norms_tracker:
                 gradient_norm = tracker.result().numpy()
-                #print("layer_name: ", layer_name)
-                #print("gradient_norm: ", gradient_norm)
                 
                 if 'dense' in layer_name:
                     if not layer_name in self.history:
@@ -54,13 +52,7 @@ class GradientMonitor(tf.keras.callbacks.Callback):
                 if 'dense' in layer_name:
                     if not layer_name in self.history:
                         self.history[f"{layer_name}"] = []
-                    self.history[f"{layer_name}"].append(gradient_norm)
-                    
-        # Save and plot gradients every k epochs
-        #if (epoch+1) % self.k == 0:
-        #    print("Plotting gradients")
-        #    #self.plot_gradients()
-        #    #self._save_gradients()
+                    self.history[f"{layer_name}"].append(gradient_norm)                   
             
     def _save_gradients(self):
         generator_path = os.path.join(self.out_dir, 'generator_gradients.npy')
@@ -77,8 +69,6 @@ class GradientMonitor(tf.keras.callbacks.Callback):
     def plot_gradients(self):
         hist = self.history
 
-        #print("hist.items(): ", hist.items())
-
         # Plot layer-specific gradient norms
         disc_layer_grad_norm_name_arr = []
         gen_layer_grad_norm_name_arr = []
@@ -91,8 +81,6 @@ class GradientMonitor(tf.keras.callbacks.Callback):
                 gen_layer_grad_norm_name_arr.append(key)
 
         #epochs = list(range(0, len(hist[disc_layer_grad_norm_name_arr[1]]) + 1, self.k))
-        
-        #print("disc_layer_grad_norm_name_arr: ", disc_layer_grad_norm_name_arr)
         if (len(disc_layer_grad_norm_name_arr) <= 5):
             disc_columns = 1
         if (len(disc_layer_grad_norm_name_arr) > 5 and len(disc_layer_grad_norm_name_arr) <= 10):
@@ -172,7 +160,6 @@ class GradientMonitor(tf.keras.callbacks.Callback):
 
         # Plot training history
         hist = self.history
-        #print("hist: ", hist)
         plt.figure(figsize=(14, 6))
         plt.subplot(1, 2, 1)
         plt.semilogy(np.array(hist['g_loss'])*4, label='Generator', alpha=0.75)
@@ -199,25 +186,30 @@ class GradientMonitor(tf.keras.callbacks.Callback):
 
 
 class ChiSquareMonitor(tf.keras.callbacks.Callback):
-    def __init__(self, monitored_model, data, target_distributions, output_path, latent_dim, d_scaler, out_dir, frequency, gan_type):
+    def __init__(self, training_models, data, target_distributions, output_path, latent_dim, d_scaler, out_dir, frequency, gan_type):
         super(ChiSquareMonitor, self).__init__()
-        self.monitored_model = monitored_model
+
         self.data = data
         self.d_scaler = d_scaler
         self.target_distributions = target_distributions
         self.output_path = output_path
-        #self.chi2_func = chi2_func
         self.frequency = frequency
         self.out_dir = out_dir
         self.chi2_values = []
         self.noise_dim = latent_dim
         self.gan_type = gan_type
 
+        if (self.gan_type == "outer"):
+            self.outer_GAN_model, self.inner_GAN_model = training_models
+        elif (self.gan_type == "inner"):
+            self.inner_GAN_model = training_models
+
+
     def calculate_chi2(self, generated_distributions, target_distributions, bins=100):
-        #chi2 = np.sum((target_distributions - generated_distributions)**2 / target_distributions)
         chi2_values = []
         num_variables = generated_distributions.shape[1]  # Assuming multi-dimensional data
-        ranges = [(1,10), (0,4), (-6,0), (0,6.5)]
+        #ranges = [(1,10), (0,4), (-6,0), (0,6.5)]
+        ranges = [(-1,1), (-1,1), (-1,1), (-1,1)]
         for i in range(num_variables):
             # Bin the generated and target distributions
             gen_hist, bin_edges = np.histogram(generated_distributions[:, i], bins=bins, density=True, range=ranges[i])
@@ -235,24 +227,21 @@ class ChiSquareMonitor(tf.keras.callbacks.Callback):
         
     def on_epoch_end(self, epoch, logs=None):
         if (epoch + 1) % self.frequency == 0:
-            #print("self.gan_type: ", self.gan_type)
             if (self.gan_type == "outer"):
-                vertex_s = self.data[:,4]
-                #print("vertex_s: ", vertex_s)
-                #print("vertex_s.shape: ", vertex_s.shape)
-                #print("vertex_s.shape[0]: ", vertex_s.shape[0])
+                vertex_s = self.data
+                vertex_s = tf.convert_to_tensor(vertex_s, dtype=tf.float32)
+                
                 noise = tf.random.normal(shape=(vertex_s.shape[0], self.noise_dim))
 
                 # Run prediction to generate GAN's output distribution
-                generated_distributions = self.monitored_model.generator.predict([vertex_s, noise], batch_size=1024)
-                
-                generated_distributions = self.d_scaler.reverse(generated_distributions)
+                unf_generated_images = self.outer_GAN_model.generator([vertex_s, noise], training=False)
+                noise = tf.random.normal(shape=(tf.shape(unf_generated_images)[0], self.noise_dim))
+                generated_distributions = self.inner_GAN_model.generator([unf_generated_images, noise], training=False)
             elif (self.gan_type == "inner"):
                 noise = tf.random.normal(shape=(self.data.shape[0], self.noise_dim))
 
                 # Run prediction to generate GAN's output distribution
-                generated_distributions = self.monitored_model.generator.predict([self.data, noise], batch_size=1024)
-                generated_distributions = self.d_scaler.reverse(generated_distributions)
+                generated_distributions = self.inner_GAN_model.generator.predict([self.data, noise], batch_size=1024)
 
             # Calculate chi^2 between generated and target distributions
             chi2_value = self.calculate_chi2(generated_distributions, self.target_distributions)
@@ -291,17 +280,20 @@ class ChiSquareMonitor(tf.keras.callbacks.Callback):
         return self.chi2_values
 
 class AccuracyMonitor(tf.keras.callbacks.Callback):
-    def __init__(self, input_model, out_dir, frequency, training_data, batch_size, noise_dim, gan_type):
+    def __init__(self, training_models, out_dir, frequency, training_data, batch_size, noise_dim, gan_type):
         super().__init__()
-        self.input_model = input_model
         self.out_dir = out_dir
-        #self.k = k
         self.accuracy_data = []
         self.frequency = frequency
         self.training_data = training_data  # (labels, images)
         self.batch_size = batch_size
         self.noise_dim = noise_dim
         self.gan_type = gan_type
+        
+        if (self.gan_type == "inner"):
+            self.inner_GAN_model = training_models
+        elif (self.gan_type == "outer"):
+            self.outer_GAN_model, self.inner_GAN_model = training_models
 
         self.history = {
             "accuracy": [],
@@ -329,44 +321,40 @@ class AccuracyMonitor(tf.keras.callbacks.Callback):
 
     def compute_predictions(self):
         # Unpack training data
-        labels, images = self.training_data
-        #print("labels.shape: ", labels.shape)
-        #print("images.shape: ", images.shape)
+        input_data, detector_data = self.training_data
         batch_size = self.batch_size
 
         # Generate noise
         noise = tf.random.normal([batch_size, self.noise_dim])
 
-        if (self.gan_type == "outer"):
-            vertex_s = labels[:,4]
-            vertex_s = tf.convert_to_tensor(vertex_s, dtype=tf.float32)
-            labels = labels[:,:-1]
-            labels = tf.convert_to_tensor(labels, dtype=tf.float32)
-            images = images[:,:-1]
-            images = tf.convert_to_tensor(images, dtype=tf.float32)
-            # Generate fake images
-            #generated_images = self.model.generator([labels[:batch_size], noise], training=False)
-            generated_images = self.model.generator([vertex_s[:batch_size], noise], training=False)
-            #print("generated_images.shape: ", generated_images.shape)
-        elif (self.gan_type == "inner"):
-            labels = tf.convert_to_tensor(labels, dtype=tf.float32)
-            images = tf.convert_to_tensor(images, dtype=tf.float32)
-            # Generate fake images
-            generated_images = self.model.generator([labels[:batch_size], noise], training=False)
-
-        # Combine real and fake images and labels
-        combined_images = tf.concat([images[:batch_size], generated_images], axis=0)
-        combined_labels = tf.concat([labels[:batch_size], labels[:batch_size]], axis=0)
-
-        # Create true labels for discriminator
+        # Create true/false labels for discriminator
         true_labels = tf.concat(
-            [tf.ones((batch_size, 1)), tf.zeros((batch_size, 1))], axis=0
+            [tf.zeros((batch_size, 1)), tf.ones((batch_size, 1))], axis=0
         )
 
-        # Get predictions from discriminator
-        predictions = self.model.discriminator(
-            [combined_labels, combined_images], training=False
-        )
+        if (self.gan_type == "outer"):
+            input_data = tf.convert_to_tensor(input_data, dtype=tf.float32)
+
+            # Generate fake images
+            unf_generated_images = self.model.generator([input_data[:batch_size], noise], training=False)
+            noise = tf.random.normal(shape=(tf.shape(unf_generated_images)[0], self.noise_dim))
+            generated_images = self.inner_GAN_model.generator([unf_generated_images[:batch_size], noise], training=False)
+
+            # Get predictions from discriminator
+            predictions = self.outer_GAN_model.discriminator(
+                [input_data[:batch_size], generated_images], training=False
+            )
+            
+        elif (self.gan_type == "inner"):
+            input_data = tf.convert_to_tensor(input_data, dtype=tf.float32)
+
+            # Generate fake images
+            generated_images = self.model.generator([input_data[:batch_size], noise], training=False)
+         
+            # Get predictions from discriminator
+            predictions = self.inner_GAN_model.discriminator(
+                [input_data[:batch_size], generated_images], training=False
+            )   
 
         # Apply sigmoid if necessary
         if (
@@ -378,6 +366,7 @@ class AccuracyMonitor(tf.keras.callbacks.Callback):
         # Convert tensors to NumPy arrays
         true_labels = true_labels.numpy().flatten()
         predicted_scores = predictions.numpy().flatten()
+        #predicted_scores = np.zeros(true_labels)
 
         return true_labels, predicted_scores
     